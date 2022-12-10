60c3c1ac06ab422eea9759ea4934230fc909b04d
Patrick Burns <burns212@gmail.com>
2022-12-10 11:58:30 -0600

    finished chapt 9

--- utils/data/eventData.js
+++ utils/data/eventData.js
@@ -7,5 +7,17 @@ const getEvents = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createEvent = (event) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/events`, {
+    method: 'POST',
+    body: JSON.stringify(event),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => resolve(response.json()))
+    .catch(reject);
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { getEvents };
+export { getEvents, createEvent };
